4ecf229d286407875fe57afab75a75077ca03364
zahra-aaa <zahrainasazizah02@gmail.com>
2024-11-06 07:20:53 +0700

    Create looping_for.py

---
+++ looping_for.py
@@ -0,0 +1,7 @@
+#Daftar nama mahasiswa
+daftar_mahasiswa = ["Tri", "kania", "Nadin", "Nanta", "Lita"]
+
+#Looping untuk mencetak nama mahasiswa
+print("Daftar Mahasiswa:")
+for mahasiswa in daftar_mahasiswa:
+      print(mahasiswa)
